463880c9eac9e6169e7fa447bcfe979c1be56b91
explorerwjy <explorerwjy@gmail.com>
2017-01-24 23:37:50 -0500

    Add ConvNets

--- Models.py
+++ Models.py
@@ -72,9 +72,109 @@ def FullyConnectNN(InputLayer, Hidden1, Hidden2, Hidden3, Hidden4, Hidden5, Hidd
 		logits = tf.matmul(hidden8, weights) + biases
 	return logits
 
-def ConvNet():
+class ConvNets():
+	def __init__():
+		pass
+	def Inference(self, InputTensor):
+		# conv1
+		with tf.variable_scope('conv1') as scope:
+			kernel = _variable_with_weight_decay('weights', shape=[5,5,3,64], stddev=5e-2, wd=0.0)
+			conv = tf.nn.conv2d(InputTensor, kernel, [1,1,1,1], padding='SAME')
+			biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.0))
+			pre_activation = tf.nn.bias_add(conv, biases)
+			conv1 = tf.nn.relu(pre_activation, name=scope.name)
+			_activation_summary(conv1)
+		# pool1
+		pool1 = tf.nn.max_pool(conv1, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME', name='pool1')
+		# norm1
+		norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001/9.0, beta=0.75, name='norm1')
 
-	return
+		# conv2
+		with tf.variable_scope('conv2') as scope:
+			kernel = _variable_with_weight_decay('weights', shape=[5,5,64,64], stddev=5e-2, wd=0.0)
+			conv = tf.nn.conv2d(InputTensor, kernel, [1,1,1,1], padding='SAME')
+			biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.1))
+			pre_activation = tf.nn.bias_add(conv, biases)
+			conv2 = tf.nn.relu(pre_activation, name=scope.name)
+			_activation_summary(conv2)
+		# norm2
+		norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001/9.0, beta=0.75, name='norm2')
+		# pool2
+		pool2 = tf.nn.max_pool(norm2, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME', name='pool2')
+		
+		# local3
+		with tf.variable_scope('local3') as scope:
+			reshape = tf.reshape(pool2, [FLAGS.batch_size, -1])
+			dim = reshape.get_shape()[1].value
+			weights = _variable_with_weight_decay('weights', shape=[dim,384], stddev=0.04, wd=0.004)
+			biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
+			local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+			_activation_summary(local3)
+
+		# local4
+		with tf.variable_scope('local4') as scope:
+			weights = _variable_with_weight_decay('weights', shape=[384, 192], stddev=0.04, wd=0.004)
+			biases = _variable_on_cpu('biases', [192], tf.constant_initializer(0.1))
+			local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
+			_activation_summary(local4)
+
+		# linear layer (WX + b)
+		with tf.variable_scope('softmax_linear') as scope:
+			weights = _variable_with_weight_decay('weights', [192, NUM_CLASSES], stddev=1/192.0, wd=0.0)
+			biases = _variable_on_cpu('biases', [NUM_CLASSES], tf.constant_initializer(0.0))
+			softmax_linear = tf.add(tf.matmul(local4, weights), biases, name=scope.name)
+			_activation_summary(softmax_linear)
+
+		return softmax_linear
+
+	def loss(self, logits, labels):
+		labels = tf.cast(labels, tf.int64)
+		cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits, name='cross_entropy_per_example')
+		cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
+		tf.add_to_collection('losses', cross_entropy_mean)
+		return tf.add_n(tf.get_collection('losses'), name='total_loss')
+
+	def Train(total_loss, global_step):
+		num_batches_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN / FLAGS.batch_size
+		decay_steps = int(num_batches_per_epoch * NUM_EPOCHS_PER_DECAY)
+		lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE, global_step, decay_steps, LEARNING_RATE_DECAY_FACTOR, staircase=True)
+		tf.scalar_summary('learning_rate', lr)
+		loss_averages_op = _add_loss_summaries(total_loss)
+
+		with tf.control_dependencies([loss_averages_op]):
+			opt = tf.train.GradientDescentOptimizer(lr)
+			grads = opt.compute_gradients(total_loss)
+
+		apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+
+		for grad, var in grads:
+			if grad is not None:
+				tf.histogram_summary(var.op.name + '/gradients', grad)
+		variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+		variable_averages_op = variable_averages.apply(tf.trainable_variables())
+
+		with tf.control_dependencies([apply_gradient_op, variable_averages_op]):
+			train_op = tf.no_op(name='train')
+		return train_op
+
+
+def _variable_on_cpu(name, shape, initializer):
+	with tf.device('/cpu:0'):
+		dtype = tf.float16 if FLAGS.use_fl16 else tf.float32
+		var = tf.get_variable(name, shape, initializer=initializer, dtype=dtype)
+	return var
+def _activation_summary(x):
+	tensor_name = re.sub('%s_[0-9]/' % TOWER_NAME, '', x.op.name)
+	tf.histogram_summary(tensor_name + '/activations', x)
+	tf.scalar_summary(tensor_name + '/sparsity', tf.nn.zero_fraction(x))
+
+def _variable_with_weight_decay(name, shape, stddev, wd):
+	dtype = tf.float16 if FLAGS.use_fl16 else tf.float32
+	var = _variable_on_cpu(name, shape, tf.truncated_normal_initializer(stddev=stddev, dtype=dtype))
+	if wd is not None:
+		weight_decay = tf.mul(tf.nn.l2_loss(var), wd, name='weight_loss')
+		tf.add_to_collection('losses', weight_decay)
+	return var
 
 def GetOptions():
 	parser = OptionParser()
