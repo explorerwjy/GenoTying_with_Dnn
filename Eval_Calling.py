2a82b4e779dddc8d3808081b304032eb25187d95
explorerwjy <explorerwjy@gmail.com>
2017-02-20 23:02:07 -0500

    eval calling

---
+++ Eval_Calling.py
@@ -0,0 +1,26 @@
+#!/home/local/users/jw/anaconda2/bin/python
+#Author: jywang	explorerwjy@gmail.com
+
+#========================================================================================================
+# Eval_Calling.py
+# Eval on Tensor caller "called" variants.
+# Need Wendow Tensor evaled from and called .txt file with GT
+#========================================================================================================
+
+from optparse import OptionParser
+
+
+
+def GetOptions():
+	parser = OptionParser()
+	parser.add_option('-','--',dest = '', metavar = '', help = '')
+	(options,args) = parser.parse_args()
+	
+	return
+
+def main():
+	
+	return
+
+if __name__=='__main__':
+	main()
