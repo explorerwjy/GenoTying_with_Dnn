ccefb67b84f69ca68ac1c489b21040a945c46068
explorerwjy <explorerwjy@gmail.com>
2017-04-24 00:49:40 -0400

    Add Continue Train

--- Training.py
+++ Training.py
@@ -273,9 +273,103 @@ def TestInput():
             coord.request_stop()
             coord.join(threads)
 
-
-"""
 def continue_train(ModelCKPT):
+    dtype = tf.float16 if FLAGS.use_fl16 else tf.float32
+    BATCH_SIZE = FLAGS.batch_size
+    TrainingHand = gzip.open(FLAGS.TrainingData, 'rb')
+    TrainingReader = RecordReader(TrainingHand)
+
+    with tf.Graph().as_default():
+        queue_input_data = tf.placeholder(
+            dtype, shape=[DEPTH * (HEIGHT + 1) * WIDTH])
+        queue_input_label = tf.placeholder(tf.int32, shape=[])
+        queue = tf.RandomShuffleQueue(capacity=FLAGS.batch_size * 10,
+                                      dtypes=[dtype,
+                                              tf.int32],
+                                      shapes=[[DEPTH * (HEIGHT + 1) * WIDTH],
+                                              []],
+                                      min_after_dequeue=FLAGS.batch_size,
+                                      name='RandomShuffleQueue')
+        enqueue_op = queue.enqueue([queue_input_data, queue_input_label])
+        dequeue_op = queue.dequeue()
+        # Get Tensors and labels for Training data.
+        data_batch, label_batch = tf.train.batch(
+            dequeue_op, batch_size=FLAGS.batch_size, capacity=FLAGS.batch_size * 4)
+        #data_batch_reshape = tf.transpose(data_batch, [0,2,3,1])
+
+        global_step = tf.Variable(0, trainable=False, name='global_step')
+
+        # Build a Graph that computes the logits predictions from the
+        # inference model.
+        convnets = Models.ConvNets()
+        logits = convnets.Inference(data_batch)
+
+        # Calculate loss.
+        loss = convnets.loss(logits, label_batch)
+
+        # Build a Graph that trains the model with one batch of examples and
+        # updates the model parameters.
+        train_op = convnets.Train(loss, global_step)
+        summary = tf.summary.merge_all()
+
+        init = tf.global_variables_initializer()
+        saver = tf.train.Saver()
+        sess = tf.Session()
+        saver.restore(sess, ModelCKPT)
+        summary_writer = tf.summary.FileWriter(log_dir, sess.graph)
+        sess.run(init)
+
+        coord = tf.train.Coordinator()
+
+        enqueue_thread = Thread(
+            target=enqueueInputData,
+            args=[
+                sess,
+                coord,
+                TrainingReader,
+                enqueue_op,
+                queue_input_data,
+                queue_input_label])
+        enqueue_thread.isDaemon()
+        enqueue_thread.start()
+
+        threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+
+        min_loss = 100
+        try:
+            for step in xrange(max_steps):
+                start_time = time.time()
+                #_, loss_value, v_step = sess.run([train_op, loss, global_step])
+                loss_value, _, v_step = sess.run([loss, train_op, global_step])
+                #_, loss_value, v_step, queue_size = sess.run([train_op, loss, global_step, queue.size()])
+                duration = time.time() - start_time
+                if (v_step) % 100 == 0 or (v_step) == max_steps:
+                    summary_str = sess.run(summary)
+                    summary_writer.add_summary(summary_str, v_step)
+                    summary_writer.flush()
+                    # Save Model only if loss decreasing
+                    if loss_value < min_loss:
+                        checkpoint_file = os.path.join(log_dir, 'model.ckpt')
+                        saver.save(
+                            sess, checkpoint_file, global_step=global_step)
+                        min_loss = loss_value
+                        print "Write A CheckPoint at %d" % (v_step)
+                    #loss_value = sess.run(loss, feed_dict=feed_dict)
+                    print 'Step %d Training loss = %.3f (%.3f sec); Saved loss = %.3f' % (v_step, loss_value, duration, min_loss)
+                elif v_step % 10 == 0:
+                    print 'Step %d Training loss = %.3f (%.3f sec)' % (v_step, loss_value, duration)
+                    # print "Queue Size", queue_size
+                    summary_str = sess.run(summary)
+                    summary_writer.add_summary(summary_str, v_step)
+                    summary_writer.flush()
+        except Exception as e:
+            coord.request_stop(e)
+        finally:
+            sess.run(queue.close(cancel_pending_enqueues=True))
+            coord.request_stop()
+            coord.join(threads)
+
+def continue_train_(ModelCKPT):
     with tf.Graph().as_default():
         print "Locating Data File"
         TrainingData = gzip.open(FLAGS.TrainingData,'rb')
@@ -336,7 +430,7 @@ def continue_train(ModelCKPT):
                 feed_dict = fill_feed_dict(data_sets_testing, tensor_placeholder, labels_placeholder)
                 loss_value = sess.run(loss, feed_dict=feed_dict)
                 print 'Step %d Test loss = %.3f (%.3f sec). Saved loss = %.3f' % (v_step, loss_value, duration, min_loss)
-"""
+
 
 
 def GetOptions():
